5ca9c9abc4cfcfaad95d49c0e75529f611c25e1c
tit <sblohin@yandex.ru>
2018-09-27 00:26:08 +0300

    fix spec example name

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,5 +2,5 @@ require_relative '../lib/main'
 
 describe :main do
   it(:modifier) { expect { modifier }.to_not raise_exception(StandardError) }
-  it(:modifier) { expect { statement }.to_not raise_exception(StandardError) }
+  it(:statement) { expect { statement }.to_not raise_exception(StandardError) }
 end
